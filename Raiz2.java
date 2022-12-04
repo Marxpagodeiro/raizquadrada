b212e2c6b198ef8e4bbc6bd849b8c9f74ebf1042
Marxpagodeiro <113648003+Marxpagodeiro@users.noreply.github.com>
2022-12-04 16:10:21 -0300

    raiz quadrada pelo método babilônico

--- Raiz2.java
+++ Raiz2.java
@@ -13,22 +13,43 @@ import java.util.Scanner;
  */
 public class Raiz2 {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int x;
-        System.out.println("Qual numero vc que saber a raiz quadrada?");
-        x=in.nextInt(); 
-        int raiz = raiz(x);
-        System.out.println(raiz);
-    }
-    public static int raiz(int x){
-        int n = 0;
-        int i = 0; 
-        while(i<=x){
-            x -= i; //Atualiza o valor de x;  x=x-i ;
-            i += 2; //Atualiza o valor de i;  i=i+2. Deste modo, i sempre será ímpar;
-            n++; //Soma mais 1 ao n;
+   public static void main(String[] args){
+        String texto = "Qual numero vc que saber a raiz quadrada?";
+        double ra = 0;// Raiz        
+        double sub = 0; // double auxiliar de substituição
+        double num = numeroUsuario(texto); // numero que o usuario deu 
+        ra = maisProximo(num); // chamando a função numero mais proximo
+        while((ra * ra) != num){
+            ra = (ra + (num/ra))/2;
+            if(ra == sub){
+                System.out.println("A raiz quadrada aproximada de " + num + " é " + ra);
+                break;
+            }
+            sub = ra;
         }
-        return n;
+        if((ra * ra) == num){
+            System.out.println("A raiz quadrada exata de " + num + " é " + ra);
+        }
+        
+    }
+   public static double numeroUsuario(String texto){
+       Scanner in = new Scanner(System.in);
+       double num;
+       System.out.println(texto);
+       num = in.nextDouble();
+       return num;
+       
+   }
+    public static double maisProximo(double vet){
+        double raiz = 1;
+        int aux = 0;
+        for(int i = 0; i < vet; i++){
+            aux = (i - 1) * (i - 1);
+            if((i * i) > vet){
+                raiz = aux;
+                break;
+            }
+        }
+        return raiz;
     }
 }
