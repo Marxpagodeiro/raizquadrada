860a6a9c23524dbccf469ed26b88f490384f16fa
Marxpagodeiro <113648003+Marxpagodeiro@users.noreply.github.com>
2022-12-06 15:36:36 -0300

    Add files via upload

--- Raiz2.java
+++ Raiz2.java
@@ -19,27 +19,26 @@ public class Raiz2 {
         double sub = 0; // double auxiliar de substituição
         double num = numeroUsuario(texto); // numero que o usuario deu 
         ra = maisProximo(num); // chamando a função numero mais proximo
-        if (num <=0){
-            System.out.println("Não é possível calcular raíz quadrada de 0 ou de número negativo");
-        }else{
-            while((ra * ra) != num){
-                ra = (ra + (num/ra))/2;
-                if(ra == sub){
-                    System.out.println("A raiz quadrada aproximada de " + num + " é " + ra);
-                    break;
-                }
-                sub = ra;
+        while((ra * ra) != num){
+            ra = (ra + (num/ra))/2;
+            if(ra == sub){
+                System.out.println("A raiz quadrada aproximada de " + num + " é " + ra);
+                break;
             }
-            if((ra * ra) == num){
-                System.out.println("A raiz quadrada exata de " + num + " é " + ra);
-            }
+            sub = ra;
         }
-    }
+        if((ra * ra) == num){
+            System.out.println("A raiz quadrada exata de " + num + " é " + ra);
+        }
+   }
    public static double numeroUsuario(String texto){
        Scanner in = new Scanner(System.in);
        double num;
        System.out.println(texto);
        num = in.nextDouble();
+       if (num < 0) {
+           num = -num;
+       }
        return num;
        
    }
